cd140ce17ca41c404d11c0de4d2082b007978faa
Msksgm <msksgm3124@gmail.com>
2021-08-25 07:39:16 +0900

    add: Yupによるバリデーションを追加

--- src/components/useFormikWithYup.tsx
+++ src/components/useFormikWithYup.tsx
@@ -1,41 +1,6 @@
 import { FC } from "react";
 import { useFormik } from "formik";
-
-type valuesType = {
-  firstName: string;
-  lastName: string;
-  email: string;
-};
-
-type errorsType = {
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-};
-
-const validate = (values: valuesType) => {
-  const errors: errorsType = {};
-
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
+import * as Yup from "yup";
 
 const SignupForm: FC = () => {
   const formik = useFormik({
@@ -44,7 +9,15 @@ const SignupForm: FC = () => {
       lastName: "",
       email: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
